dba24dc821ccd1eda8efdfbb4fda28f445f2ddd0
andraagus <andra.agus@gmail.com>
2026-03-06 11:11:28 +0700

    update vaalidasi coraccount

--- validasi-data-confins.bat
+++ validasi-data-confins.bat
@@ -1,4 +1,7 @@
 @echo off
+python "%~dp0validasi-data-coreaccount.py"
 python "%~dp0validasi-data-customer.py"
 python "%~dp0validasi-data-customerpersonal.py"
+python "%~dp0validasi-data-custcorporate.py"
+python "%~dp0validasi-data-custcorpmanagement.py"
 pause
